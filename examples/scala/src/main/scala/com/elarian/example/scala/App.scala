71621a7acddc6681e231fb69ab263b584b3e105d
Salama Balekage <aksalj@aksalj.com>
2020-07-22 17:22:08 +0300

    Prep scala example

--- examples/scala/src/main/scala/com/elarian/example/scala/App.scala
+++ examples/scala/src/main/scala/com/elarian/example/scala/App.scala
@@ -1,42 +1,18 @@
-package com.elarian.example.scala;
+package com.elarian.example.scala
 
-import com.elarian.hera.Elarian;
-import com.elarian.hera.proto.GrpcWebServiceGrpc;
-import com.elarian.hera.proto.GrpcWebServiceGrpc.GrpcWebServiceBlockingStub;
-import com.elarian.hera.proto.Web.*;
-import io.grpc.stub.StreamObserver;
+import com.elarian.hera.Elarian
+import com.elarian.hera.proto.Web.GetCustomerStateRequest
 
 
 object App {
-    def main(args: Array[String]) = {
-        GrpcWebServiceBlockingStub instance = Elarian.newInstance("test_api_key", true);
-
-        GetCustomerStateRequest req =  GetCustomerStateRequest
+    def main(args: Array[String]): Unit = {
+        val instance = Elarian.newInstance("test_api_key", true);
+        val req =  GetCustomerStateRequest
                 .newBuilder()
                 .setCustomerId("fake_id")
                 .setAppId("test_app")
                 .build();
-        GetCustomerStateReply res = instance.getCustomerState(req);
-        System.err.println(res.toString());
-
-        GrpcWebServiceGrpc.GrpcWebServiceStub asyncInstance = Elarian.newAsyncInstance("test_api_key", true);
-        asyncInstance.getCustomerState(req, new StreamObserver<GetCustomerStateReply>() {
-            @Override
-            public void onNext(GetCustomerStateReply value) {
-                System.err.println(value.toString());
-            }
-
-            @Override
-            public void onError(Throwable t) {
-                t.printStackTrace();
-            }
-
-            @Override
-            public void onCompleted() {
-
-            }
-        });
-
-        Thread.sleep(5000);
+        val res = instance.getCustomerState(req)
+        System.err.println(res.toString)
     }
 }
