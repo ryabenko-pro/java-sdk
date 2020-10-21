62d49bdacb62da69ebba3a321e4fe4f2816f7b7e
Salama AB <aksalj@aksalj.com>
2020-10-21 18:07:53 +0300

    Update kotlin sample

--- examples/kotlin/src/main/kotlin/com/elarian/example/kotlin/App.kt
+++ examples/kotlin/src/main/kotlin/com/elarian/example/kotlin/App.kt
@@ -1,22 +1,25 @@
 package com.elarian.example.kotlin
 
+import kotlinx.coroutines.*
 import com.elarian.hera.proto.Web.*
 import com.elarian.hera.proto.Common.*
 import com.elarian.hera.Elarian
 
 fun main() {
     val elarian = Elarian.newInstance("test_api_key")
-    val req = GetCustomerStateRequest
+    runBlocking {
+        val req = StreamNotificationRequest
             .newBuilder()
             .setOrgId("test_org")
-            .setCustomerNumber(
-                    CustomerNumber
-                            .newBuilder()
-                            .setNumber("+254720000000")
-                            .setProvider(CustomerNumberProvider.CUSTOMER_NUMBER_PROVIDER_TELCO)
-                            .build()
-            )
+            .setAppId("test_app")
             .build()
-    val res = elarian.getCustomerState(req)
-    print(res)
+
+        val stream = elarian.streamNotifications(req)
+        while(stream.hasNext()) {
+            val notif = stream.next()
+            println(notif)
+        }
+    }
+
+    Thread.sleep(20000L)
 }
